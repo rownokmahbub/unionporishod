d68965b43083f71d87644c470edbb9c690464aed
Rownok <rownok2001@gmail.com>
2023-05-27 22:20:32 +0600

    update taxentry

---
+++ taxentryreg.php
@@ -0,0 +1,40 @@
+<?php
+     include '_dbconnect.php';/// received data collect
+      session_start();
+    // to receive the post data we need to call $_POST variable
+    // $_POST represents an associative array
+
+    
+       
+        /// to establish a connection with database server
+        
+       
+        $var2=$_POST['holdingno'];
+        $var3=$_POST['name'];
+        $var4=$_POST['fname'];
+        $var5=$_POST['gram'];
+        $var6=$_POST['ward']; 
+        
+        
+         $var8=$_POST['tax'];
+         $var9=$_POST['mnum'];
+         
+         
+     
+   $query=" INSERT INTO `taxentry`(`holdingno`, `name`, `fatherhusband`, `gram`, `ward`, `totaltax`, `mobilenum`) VALUES ('$var2','$var3','$var4','$var5','$var6','$var8','$var9')";
+
+	if(mysqli_query($con, $query)){
+    
+ $_SESSION['tax_entry'] = 'success';
+ echo 'inset successfull';
+  header('location:taxentry.php');
+      
+	 
+        }
+        else{
+      $_SESSION['tax_entry'] = 'error';
+      header('location:taxaday.php');
+        }
+
+       // echo $query;
+ ?>
--- userhome.php
+++ userhome.php
@@ -28,93 +28,71 @@
 <body>
 
 <!--navigation-->
-<div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-    <div class="relative flex items-center justify-between">
-        <a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
-            <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-                <rect x="3" y="1" width="7" height="12"></rect>
-                <rect x="3" y="17" width="7" height="6"></rect>
-                <rect x="14" y="1" width="7" height="6"></rect>
-                <rect x="14" y="11" width="7" height="12"></rect>
-            </svg>
-            <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Blogger</span>
-        </a>
-        <ul class="flex items-center hidden space-x-8 lg:flex">
-            <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
-            <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
-            <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
-            <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
-        </ul>
-        <ul class=" items-center hidden space-x-8 lg:flex">
-            <li>
-                <form action="userlogout.php">
-
-                    <button class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide
-                 text-white transition duration-200 rounded-full shadow-md bg-purple-400
-                 hover:bg-purple-700 focus:shadow-outline focus:outline-none" type='submit'>Log Out</button>
-                </form>
-
-            </li>
-        </ul>
-        <!-- Mobile menu -->
-        <div class="lg:hidden">
-            <button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
-                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
-                    <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
-                    <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
-                </svg>
-            </button>
-
-            <div class="absolute top-0 left-0 w-full">
-                <div class="p-5 bg-white border rounded shadow-sm">
-                    <div class="flex items-center justify-between mb-4">
-                        <div>
-                            <a href="#" aria-label="Company" title="Company" class="inline-flex items-center">
-                                <svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-                                    <rect x="3" y="1" width="7" height="12"></rect>
-                                    <rect x="3" y="17" width="7" height="6"></rect>
-                                    <rect x="14" y="1" width="7" height="6"></rect>
-                                    <rect x="14" y="11" width="7" height="12"></rect>
-                                </svg>
-                                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Blogger</span>
-                            </a>
-                        </div>
-                        <div>
-                            <button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-                                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-                                    <path
-                                            fill="currentColor"
-                                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                                    ></path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <nav>
-                        <ul class="space-y-4">
-                            <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
-                            <li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
-                            <li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
-                            <li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
-                            <li>
-                                <a
-                                        href="login2.php"
-                                        class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                                        aria-label="Sign up"
-                                        title="Sign up"
-                                >
-                                    Sign up
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-
+<nav class="bg-white shadow-lg">
+    <div class="max-w-6xl mx-auto px-4">
+      <div class="flex justify-between">
+        <div class="flex space-x-7">
+          <div>
+            <!-- Website Logo -->
+            <a href="#" class="flex items-center py-4 px-2">
+              <img src="Assets/logo.png" alt="Logo" class="h-8 w-8 mr-2">
+              <span class="font-semibold text-gray-500 text-lg">UP </span>
+            </a>
+          </div>
+          <!-- Primary Navbar items -->
+          <div class="hidden md:flex items-center space-x-1">
+            <a href="taxentry.php" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Taxentry</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Services</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">About</a>
+            <a href="" class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
+          </div>
         </div>
+        <!-- Secondary Navbar items -->
+        <div class="hidden md:flex items-center space-x-3 ">
+  
+          
+
+<button class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide
+text-white transition duration-200 rounded-full shadow-md bg-purple-400
+hover:bg-purple-700 focus:shadow-outline focus:outline-none" type='submit'>Log Out</button>
+</form>
+        </div>
+        <!-- Mobile menu button -->
+        <div class="md:hidden flex items-center">
+          <button class="outline-none mobile-menu-button">
+          <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+            x-show="!showMenu"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+        </div>
+      </div>
     </div>
-</div>
+    <!-- mobile menu -->
+    <div class="hidden mobile-menu">
+      <ul class="">
+        <li class="active"><a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
+        <li><a href="#services" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
+        <li><a href="#about" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
+        <li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+      </ul>
+    </div>
+    <script>
+      const btn = document.querySelector("button.mobile-menu-button");
+      const menu = document.querySelector(".mobile-menu");
+
+      btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+      });
+    </script>
+  </nav>
 
 
 <!--create blog-->
