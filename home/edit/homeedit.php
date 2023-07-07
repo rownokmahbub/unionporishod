04009a63e77a062b845e1d571fe4248c61d8a64e
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-07 08:07:36 +0600

    home edit done

--- home/edit/homeedit.php
+++ home/edit/homeedit.php
@@ -117,11 +117,12 @@
 </head>
 
 <body class="md:px-32 mx-auto  py-20">
-    <a href="../home.php" class="text-center font-semibold flex gap-3 items-center">
+<a href="../home.php" class="text-center bg-white font-semibold flex gap-3 items-center sticky top-0 left-0 w-full py-5 px-5">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M19 12H6M12 5l-7 7 7 7" />
         </svg>
         Back</a>
+   
     <p class="text-center font-semibold text-2xl md:text-3xl">ইউনিয়ন এর পরিচিতি এডিট করুণ </p>
     <div class="container mt-5">
     <table id="example" class="display rounded-md overflow-hidden" style="width:100%">
--- home/edit/uniondetailsedit.php
+++ home/edit/uniondetailsedit.php
@@ -31,6 +31,7 @@ include '../../_dbconnect.php';
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>    
 Back</a>
 <p class="text-center font-semibold text-2xl md:text-4xl">ইউনিয়ন এর তথ্য যোগ করুণ </p>
+<img class="my-5 rounded-xl shadow-lg" src="../../Assets/union.png" alt="">
     <?php
 
         $kid= $result['id'];
