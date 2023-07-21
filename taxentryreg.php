886d85e75f6922b37b403932752c2eec6bbf48ad
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-21 08:52:59 +0600

    Update taxentryreg.php

--- taxentryreg.php
+++ taxentryreg.php
@@ -4,10 +4,9 @@
     // to receive the post data we need to call $_POST variable
     // $_POST represents an associative array
 
-    
-       
-        /// to establish a connection with database server
-        
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+   
        
         $var1=$_POST['holdingno'];
         $var2=$_POST['name'];
@@ -16,22 +15,28 @@
         $var5=$_POST['ward']; 
         $var6=$_POST['housetype']; 
         $var7=$_POST['roomno']; 
-         $var8=$_POST['tax'];
-         $var9=$_POST['mnum'];
+        $var8=$_POST['tax'];
+        $var9=$_POST['mnum'];
          
          
      
-   $sql="INSERT INTO `taxentry`(`holdingno`, `name`, `fatherhusband`, `gram`, `ward`, `housetype`, `roomno`, `totaltax`, `voterid`) VALUES ('$var1','$var2','$var3','$var4','$var5','$var6','$var7','$var8','$var9'";
+   $sql="INSERT INTO `taxentry`(`holdingno`, `name`,`fatherhusband`, `gram`, `ward`, `housetype`, `roomno`, `totaltax`, `voterid`) VALUES ('$var1','$var2','$var3','$var4','$var5','$var6','$var7','$var8','$var9'";
 
-   if(mysqli_query($con, $sql)){
+   
 
-    print_r(error_get_last());
-	  header('location:taxentry.php');
-        }
-        else{
-          
-	 header('location:userhome.php');
-        }
+if(mysqli_query($con, $sql)){
+
+
+header("Location:taxentry.php");
+      
+} else{
+   echo "ERROR: Hush! Sorry $sql. "
+       . mysqli_error($con);
+}
+
+// Close conection
+mysqli_close($con);
+}
 
        // echo $query;
  ?>
