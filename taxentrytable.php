4f7e224c8777e9d1ce5a4f8dd71bcd24c0c37a8a
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-25 19:44:11 +0600

    rtr

--- taxentrytable.php
+++ taxentrytable.php
@@ -102,7 +102,7 @@
         <a href='taxentryedit.php?id=<?php echo $id; ?>' class='px-3 py-2 cursor-pointer rounded-xl text-white text-sm  bg-green-700 hover:bg-green-600  duration-300 transition flex items-center gap-1'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M20 14.66V20a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h5.34"></path><polygon points="18 2 22 6 12 16 8 16 8 12 18 2"></polygon></svg>    
         </a>
-        <a href='taxentrydelete.php?id=<?php echo $id; ?>' class='px-3 py-2 cursor-pointer rounded-xl text-white text-sm  bg-red-700 hover:bg-red-600  duration-300 transition flex items-center gap-1'>
+        <a href='.php?id=<?php echo $id; ?>' class='px-3 py-2 cursor-pointer rounded-xl text-white text-sm  bg-red-700 hover:bg-red-600  duration-300 transition flex items-center gap-1'>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>  
         </a>
         </div>
