02d2c5a83bb31128d67840c9dcf0c534fb72e89f
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-29 08:02:34 +0600

    Update porishodborgo.php

--- porishodborgo.php
+++ porishodborgo.php
@@ -57,8 +57,13 @@ include './navbar.php'
             <img class="w-full max-h-56  min-h-28" src="./Assets/image/<?php echo $row['filename']; ?>">
  
  
-           <div class='flex flex-col items-center -space-y-2'>
-           <p class="text-xl capitalize font-semibold px-2 py-2"><?php echo $row['name']; ?></p>
+           <div class='flex flex-col items-center -space-y-1'>
+           
+           <div class='flex items-center gap-2 mt-1'>
+           <p class="text-xl capitalize font-semibold "><?php echo $row['slno']; ?></p>
+           <p class="text-xl capitalize font-semibold "><?php echo $row['name']; ?></p>
+           </div>
+  
             <p class="text-md  px-2 py-2"><?php echo $row['mobileno']; ?></p>
            </div>
       
