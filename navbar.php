cec1a8018aaec8315465e8efe72dd04810be859c
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-07-29 12:21:09 +0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="index.php" class="flex items-center">
           <img src="Assets/logo.png" class="h-8 mr-3" alt="logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap ">তিতুদহ</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap ">তিতুদহ ইউপি</span>
       </a>
       <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-dropdown" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
